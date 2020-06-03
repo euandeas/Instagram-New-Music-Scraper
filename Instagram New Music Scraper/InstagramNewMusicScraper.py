cdda0c4c49448cd96a366ad20c434bb17f876280
Euan Deas <euandeas@hotmail.com>
2020-06-03 17:56:23 +0100

    Added classifying and saving test data

--- Instagram New Music Scraper/InstagramNewMusicScraper.py
+++ Instagram New Music Scraper/InstagramNewMusicScraper.py
@@ -1,8 +1,9 @@
 from InstagramScraper import InstagramScraper
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import json
 
-def preparedata(sentence):
+def ProcessData(sentence):
     tokens= word_tokenize(sentence)
     tokens = [w.lower() for w in tokens]
 
@@ -11,14 +12,34 @@ def preparedata(sentence):
     stop_words = set(stopwords.words('english'))
     return [w for w in symbolsremoved if not w in stop_words]
 
+def SaveTestData(data):
+    file = open("TestData.txt", w)
+    json.dump(data, file)
+    file.close()
+
+def ReadData():
+    file = open("TestData.txt")
+    data = json.load(file)
+    return data
+
+def ManualClassify(data):
+    for x in data:
+        x.append(input())
+    print("CLASSIFICATION COMPLETE")
+    show = input("Show resulting data? y/n:")
+    if show == "y":
+        for y in data:
+            print(y)
+    return data
+
 scraper = InstagramScraper()
 data = scraper.scrapedata(100)
 del scraper
 
-cleandata = []
+processeddata = []
 
 for x in data:
-    cleandata.append(preparedata(x[1]))
+    processeddata.append(processedata(x[1]))
 
 print(cleaneddata)
 input()
