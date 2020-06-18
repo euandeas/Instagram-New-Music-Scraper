ba62e6d63186c882c7982c6ebe5577c7f4c76bd1
Euan Deas <euandeas@hotmail.com>
2020-06-18 16:21:05 +0100

    small changes

--- Instagram New Music Scraper/InstagramNewMusicScraper.py
+++ Instagram New Music Scraper/InstagramNewMusicScraper.py
@@ -10,9 +10,9 @@ def ProcessData(sentence):
     tokens = [w.lower() for w in tokens]
 
     symbolsremoved = [word for word in tokens if word.isalpha()]
-    
-    stop_words = set(stopwords.words('english'))
-    return [w for w in symbolsremoved if not w in stop_words]
+    stopwordsremoved = [w for w in symbolsremoved if not w in set(stopwords.words('english'))]
+  
+    return stopwordsremoved
 
 def SaveTestData(data):
     file = open("TestData.txt", w)
@@ -49,17 +49,7 @@ processeddata = []
 for x in data:
     processeddata.append(ProcessData(x[1]))
 
-all_words = []
-for s in processeddata:
-    for w in s:
-        all_words.append(w)
 
-for a in data:
-    print(a)
-print(all_words)
-
-all_words = nltk.FreqDist(all_words)
-print(all_words.most_common(10))
 input()
 
 
