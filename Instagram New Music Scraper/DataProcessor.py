6b2fbb441f1019c5a42311e720024b094dd50ed6
Euan Deas <euandeas@hotmail.com>
2020-07-26 21:32:57 +0100

    started streamlining the TFIDF conversion

--- Instagram New Music Scraper/DataProcessor.py
+++ Instagram New Music Scraper/DataProcessor.py
@@ -1,6 +1,7 @@
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def Process(sentence):
     tokens= word_tokenize(sentence)
@@ -10,3 +11,19 @@ def Process(sentence):
     stopwordsremoved = [w for w in symbolsremoved if not w in set(stopwords.words('english'))]
   
     return stopwordsremoved
+
+class TFIDFConverter():
+
+    tfidfconverter = TfidfVectorizer(analyzer='word',tokenizer=dummy_fun,preprocessor=dummy_fun,token_pattern=None)
+
+    def Fit_Transform(self, data):
+        return self.tfidfconverter.fit_transform(data).toarray()
+    
+    def Transform(self):
+        return self.tfidfconverter.transform(processed).toarray()
+
+    def SaveVectorizer(self):
+        pickle.dump(self.tfidfconverter, open("tfidfvectorizer.p", 'wb'))
+
+    def ReadVectorizer(self):
+        self.tfidfconverter = pickle.load(open("tfidfvectorizer.p", 'rb'))
